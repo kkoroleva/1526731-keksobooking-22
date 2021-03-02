d1ba089c244f4917a0da7c364181a8151f3ba18c
kkoroleva <korolevakarinka22@gmail.com>
2021-03-02 16:42:57 +0300

    Hate linter

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@
 
 const getRandomFloat = (min = 0, max = 0, roundConst = 0) => {
   if (min >= max || roundConst < 0 || min < 0 || max < 0) {
-    console.log('Некорректные входные данные.');
-    return 'Error';
+    return 'Error. Некорректные входные данные';
   }
   roundConst = Math.ceil(roundConst);
   return (Math.random() * (max - min) + min).toFixed(roundConst);
@@ -11,13 +10,14 @@ const getRandomFloat = (min = 0, max = 0, roundConst = 0) => {
 
 const getRandomInt = (min = 0, max = 0) => {
   if (min >= max || min < 0 || max < 0) {
-    console.log('Некорректные входные данные.');
-    return 'Error';
+    return 'Error. Некорректные входные данные';
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+getRandomFloat(1,100, 5);
+getRandomInt(1,100);
 
 
